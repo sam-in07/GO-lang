212b7b1559f636c903581839ccb41136908afc4a
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-23 06:03:11 +0600

    didddi

--- Backend/main.go
+++ Backend/main.go
@@ -1,10 +1,30 @@
-package main 
+package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter , r *http.Request){
+   fmt.Fprint(w,"Hellow World")
+}
+
+/*
+w http.ResponseWriter → used to send response back to browser
+r *http.Request → contains request data (URL, headers, etc.)
+*/
 
 
 func main()  {
-	mux := http.NewServeMux()
+	mux := http.NewServeMux() //req router 
 
-	mux.Handle("/hellow",)
-} 
+	mux.HandleFunc("/hellow",helloHandler)
+}  
+
+/*
+mux → waiter (routes orders)
+/hellow → menu item
+helloHandler → chef
+fmt.Fprint → serving food
+
+*/
