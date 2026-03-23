66378ed30a3e3b897bc698b772ae5d216a2dd111
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-23 10:10:30 +0600

    Goseerver1

--- Backend/main.go
+++ Backend/main.go
@@ -9,6 +9,10 @@ func helloHandler(w http.ResponseWriter , r *http.Request){
    fmt.Fprint(w,"Hellow World")
 }
 
+func aboutHNandler(w http.ResponseWriter , r *http.Request){
+   fmt.Fprint(w,"Hellow I;m asasdjknsd")
+}
+
 /*
 w http.ResponseWriter → used to send response back to browser
 r *http.Request → contains request data (URL, headers, etc.)
@@ -19,6 +23,18 @@ func main()  {
 	mux := http.NewServeMux() //req router 
 
 	mux.HandleFunc("/hellow",helloHandler)
+	mux.HandleFunc("/about",aboutHNandler)
+
+	fmt.Println("seever running on:3000")
+
+	err := http.ListenAndServe(":3000",mux)
+
+	if err != nil {
+		fmt.Println("error stRTING server ", err)
+	}
+
+
+	 
 }  
 
 /*
